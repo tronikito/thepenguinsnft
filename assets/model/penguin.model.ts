a313f60639bfaa3b4f3c31161fb3da21ef658718
kevin <kvn2018@gmail.com>
2022-03-02 22:10:20 +0100

    Auto-generated commit [ci skip]

---
+++ assets/model/penguin.model.ts
@@ -0,0 +1,11 @@
+export class penguin {
+  //url!: string;
+  qID!: string;
+  color1!: string;
+  color2!: string;
+  color3!: string;
+  hat!: string;
+  eyes!: string;
+  tattoo!: string;
+  item!: string;
+}
---
+++ assets/model/penguins.model.ts
@@ -0,0 +1,6 @@
+import { penguin } from './penguin.model';
+
+export class penguins {
+  //url!: string;
+  penguins!: penguin[];
+}
